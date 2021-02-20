31265fa6de46a56f940f18fe17b9c76d28c54eba
Jon <jon@wearesupereffective.com>
2021-02-20 11:20:43 -0500

    Add CMYK to RGB conversion

--- src/colorutils.ts
+++ src/colorutils.ts
@@ -4,6 +4,7 @@ export {
 } from './colorTypes';
 
 export { default as sanitizeHex } from './sanitizeHex';
+export { default as cmykToRgb } from './cmykToRgb';
 export { default as hexToRgb } from './hexToRgb';
 export { default as hsvToRgb } from './hsvToRgb';
 export { default as hexToHsv } from './hexToHsv';
